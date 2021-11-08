1fb6af9e28cc4628ca8c3ce977672f2c9beb26b3
hksompaakter <20mh1a0503@acoe.edu.in>
2021-11-08 14:37:26 +0000

    list

--- Continue.py
+++ Continue.py
@@ -1,7 +1,6 @@
 i=1
 while i<=100:
-    if i==5:
+    if i == 20:
         continue
     print(i)
     i=i+1
-print(i)
---
+++ list.py
@@ -0,0 +1,3 @@
+subjects=["Bangla","English","Mathe","Islamic"]
+print(subjects)
+print(subjects[3:])
